9a3e854efd9471ff76ca1465a968bbf1276ebb7f
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-07 14:15:18 +0800

    Functional clock + comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Clock extends React.Component {
+    
+
     // class constructor that assigns the initial this.state
     constructor(props) {
         //pass props to the base constructor
@@ -11,6 +13,32 @@ class Clock extends React.Component {
         this.state = {
             date: new Date(),
         };
+    }
+
+    //Called immediately after a component is mounted. Setting state here will trigger re-rendering.
+    //The componentDidMount() method runs after the component output has been rendered to the DOM    
+    componentDidMount() {
+        //this.props is set up by React itself and this.state has a special meaning
+        //you are free to add additional fields to the class manually 
+        //if you need to store something that doesn’t participate in the data flow (like a timer ID)
+        this.timerID = setInterval(
+            //call the tick() function every 1000ms
+            //by passing an anon function to tick()
+            () => this.tick(),
+            500
+        );
+    }
+
+    //Called immediately before a component is destroyed
+    componentWillUnmount() {
+        //will tear down the timer in the componentWillUnmount() lifecycle method
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
     }
 
     render() {

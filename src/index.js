0f07ca07845b0ec0ee22d8080c1a3db3cd088c11
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-07 13:29:26 +0800

    fucntions and how they work, i think

--- src/index.js
+++ src/index.js
@@ -6,12 +6,66 @@ function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
 
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+            width="64"
+        />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: "Comment Text",
+    author: {
+        avatarUrl: "https://avatars3.githubusercontent.com/u/17591127?s=460&v=4",
+        name: "Some Name",
+    }
+}
+
 function App() {
     return (
         <div>
             <Welcome name="Name1" />
             <Welcome name="Name2" />
-            <Welcome name="Name3" />
+            <Welcome name={comment.author.name} />
+            <hr />
+            <Comment 
+                date={comment.date}
+                text={comment.text}
+                author={comment.author}
+            />
         </div>
     );
 }

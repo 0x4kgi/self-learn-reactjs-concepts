788c615e6302d3f4db880f93cb8577d1ace1a5fb
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-07 10:18:26 +0800

    Functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = <b>Hello world!</b>;
+const user = {
+    firstName: "First",
+    lastName: "Last"
+}
 
-ReactDOM.render(element, document.getElementById('root'));
+function formatName(user) {
+    return user.firstName + "-" + user.lastName;
+}
+
+
+const imageURL = "https://i.ytimg.com/vi/q38Y5FLK63k/maxresdefault.jpg";
+
+//you must have a parent tag in order to ad several tags inside
+const combination = (
+    <div>
+        <b>Hello world! ...and {formatName(user)}</b><br />
+        <img src={imageURL} alt="nothing"/>
+    </div>
+);
+
+ReactDOM.render(combination, document.getElementById('root'));

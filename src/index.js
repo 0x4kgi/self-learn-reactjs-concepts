820bb148632c61cf727cf6a948e8d593005f900e
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-07 10:43:34 +0800

    Basic Components??

--- src/index.js
+++ src/index.js
@@ -2,24 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const user = {
-    firstName: "First",
-    lastName: "Last"
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
 }
 
-function formatName(user) {
-    return user.firstName + "-" + user.lastName;
+function App() {
+    return (
+        <div>
+            <Welcome name="Name1" />
+            <Welcome name="Name2" />
+            <Welcome name="Name3" />
+        </div>
+    );
 }
 
-
-const imageURL = "https://i.ytimg.com/vi/q38Y5FLK63k/maxresdefault.jpg";
-
-//you must have a parent tag in order to ad several tags inside
-const combination = (
-    <div>
-        <b>Hello world! ...and {formatName(user)}</b><br />
-        <img src={imageURL} alt="nothing"/>
-    </div>
-);
-
-ReactDOM.render(combination, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

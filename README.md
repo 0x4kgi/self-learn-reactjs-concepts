c965d90ac96f818e0a4217d0fd7ddb32b02e4e03
[deleted] <17591127+0x4kgi@users.noreply.github.com>
2019-11-08 14:35:28 +0800

    Actual combinations (#3)
    Combined everything to origin/master.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ yeah, repo name is literally the contents
 branches are the "chapters" of learned things
 
 yeah.
+
+## Branches are:
+ - Master: combination of everything in one page
+ - 0/Hello-World: Just a simple output of a string
+ - 1/Basic-Functions: functions()
+ - 2/State-and-Lifecycle: displays a clock where it ticks per second
+ - 3/Events: button that does something simple
+ - 4/Ajax: pulls data from safebooru.donmai.us (this is an api that I find simple to use, haha)
--- src/index.js
+++ src/index.js
@@ -1,73 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './hello.js';
+import Clock from './clock.js';
+import Toggle from './trigger.js';
+import SafebooruAjax from './ajaxTest.js';
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
-
-function formatDate(date) {
-    return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-            width="64"
-        />
-    );
-}
-
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">
-                {props.user.name}
+class WebpageRender extends React.Component {    
+    render() {
+        return (
+            <div className="base-div">
+                <div>
+                    <small>From hello.js</small><br/>
+                    <Hello />               
+                </div>
+                <hr />
+                <div>
+                    <small>From clock.js</small><br/>
+                    <Clock />
+                </div>
+                <hr />
+                <div>
+                    <small>From trigger.js</small><br/>
+                    <Toggle />
+                </div>
+                <hr />
+                <div>
+                    <small>From ajaxTest.js</small><br/>
+                    <SafebooruAjax />
+                </div>
             </div>
-        </div>
-    );
-}
-
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
-}
-
-const comment = {
-    date: new Date(),
-    text: "Comment Text",
-    author: {
-        avatarUrl: "https://avatars3.githubusercontent.com/u/17591127?s=460&v=4",
-        name: "Some Name",
+        );
     }
 }
 
-function App() {
-    return (
-        <div>
-            <Welcome name="Name1" />
-            <Welcome name="Name2" />
-            <Welcome name={comment.author.name} />
-            <hr />
-            <Comment 
-                date={comment.date}
-                text={comment.text}
-                author={comment.author}
-            />
-        </div>
-    );
-}
+ReactDOM.render(<WebpageRender />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));

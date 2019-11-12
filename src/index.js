ef447fa17325c4791e64e080dd37a1f15f9b13c1
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-12 09:32:28 +0800

    Added toggles to only show what modules to use

--- src/index.js
+++ src/index.js
@@ -6,30 +6,44 @@ import Clock from './clock.js';
 import Toggle from './trigger.js';
 import SafebooruAjax from './ajaxTest.js';
 
-class WebpageRender extends React.Component {    
+class WebpageRender extends React.Component {   
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedApp: 0,
+            appArray: [
+                <Hello />,
+                <Clock />,
+                <Toggle />,
+                <SafebooruAjax />,
+            ]
+        }        
+    }
+
+    handleClick(i) {
+        this.setState({
+            selectedApp: i,
+        })
+    }
+    
     render() {
+        const s = this.state;
+        let whatApp = s.appArray[s.selectedApp];
+
         return (
             <div className="base-div">
-                <div>
-                    <small>From hello.js</small><br/>
-                    <Hello />               
+                <div className="top-nav">
+                    <button onClick={() => this.handleClick(0)}>hello.js</button>
+                    <button onClick={() => this.handleClick(1)}>clock.js</button>
+                    <button onClick={() => this.handleClick(2)}>trigger.js</button>
+                    <button onClick={() => this.handleClick(3)}>ajaxTest.js</button>
+                    <hr />
                 </div>
-                <hr />
-                <div>
-                    <small>From clock.js</small><br/>
-                    <Clock />
-                </div>
-                <hr />
-                <div>
-                    <small>From trigger.js</small><br/>
-                    <Toggle />
-                </div>
-                <hr />
-                <div>
-                    <small>From ajaxTest.js</small><br/>
-                    <b>Branch <a href="https://github.com/0x4kgi/self-learn-reactjs-concepts/tree/4/Ajax">4/Ajax</a> has more features!</b><br />
-                    <SafebooruAjax />
-                </div>
+                <div>                    
+                    {whatApp}              
+                </div>                
             </div>
         );
     }

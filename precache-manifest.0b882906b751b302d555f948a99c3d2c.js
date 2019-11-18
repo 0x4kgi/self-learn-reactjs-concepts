b2f2ea42e2f0ed6d383bf136df734c86912008af
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-18 17:08:20 +0800

    Updates

---
+++ precache-manifest.0b882906b751b302d555f948a99c3d2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f193e87757ef65861906b633cdc697d",
+    "url": "/self-learn-reactjs-concepts/index.html"
+  },
+  {
+    "revision": "5bc306f60b72bef4d4b5",
+    "url": "/self-learn-reactjs-concepts/static/css/main.e5d4ec88.chunk.css"
+  },
+  {
+    "revision": "0d54a4137839f47c45da",
+    "url": "/self-learn-reactjs-concepts/static/js/2.e265c73c.chunk.js"
+  },
+  {
+    "revision": "5bc306f60b72bef4d4b5",
+    "url": "/self-learn-reactjs-concepts/static/js/main.9e56cc5a.chunk.js"
+  },
+  {
+    "revision": "6b693dc9ed361cf76a0e",
+    "url": "/self-learn-reactjs-concepts/static/js/runtime-main.69f616f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/self-learn-reactjs-concepts/precache-manifest.968d2bf7a535b9a6872118020bf9ec5b.js"
+  "/self-learn-reactjs-concepts/precache-manifest.0b882906b751b302d555f948a99c3d2c.js"
 );
 
 self.addEventListener('message', (event) => {

727f68f8352ceba5f86cf68557ab0fe0c0a27187
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-18 16:56:16 +0800

    updated safebooru demo

--- src/apps/ajax/safebooru/thumbnails.js
+++ src/apps/ajax/safebooru/thumbnails.js
@@ -7,24 +7,55 @@ export default class RenderThumbs extends React.Component {
         this.state = {
             isLoaded: false,
             error: null,
-            limit: this.props.limit,
+            limit: 10,
+            tags: "",
+            page: 1,
             data: [],
         }
     }
 
     componentDidMount() {
         this._isMounted = true;
-        fetch('https://safebooru.donmai.us/posts.json?tags=scenery&limit=' + this.state.limit)
+
+        this.setState({
+            limit: this.props.limit,
+            tags: this.props.tags,
+            page: this.props.page,
+        }, this.apiGrab);
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    apiGrab() {
+        let limit = this.props.limit;
+        let tags = this.props.tags;
+        let page = this.props.page;
+
+        const apiURL = `https://safebooru.donmai.us/posts.json?tags=${tags}&limit=${limit}&page=${page}`;
+
+        fetch(apiURL)
             .then(res => res.json())
             .then(
                 (result) => {
                     if (this._isMounted) {
-                        this.setState({
-                            isLoaded: true,
-                            data: result,
-                        });
-                    }
-                    
+                        let responseState = {};
+
+                        if (result.message && !result.status) {
+                            responseState = {
+                                isLoaded: true,
+                                error: result.message,
+                            };
+                        } else {
+                            responseState = {
+                                isLoaded: true,
+                                data: result,
+                            };
+                        }
+
+                        this.setState(responseState);
+                    }                    
                 },
                 (error) => {
                     this.setState({
@@ -35,12 +66,8 @@ export default class RenderThumbs extends React.Component {
             );
     }
 
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
-
     render() {
-        const {error, isLoaded, data} = this.state;
+        const {error, isLoaded, data, tags} = this.state;
 
         if (error) {
             return (
@@ -56,16 +83,38 @@ export default class RenderThumbs extends React.Component {
             );
         }
 
+        let status;
+        if (tags.length === 0) {
+            status = 'Showing the most recent posts';
+        } else {
+            let tagGroup = tags.split(' ');
+            let tagsWithLinks = tagGroup.map(item => (
+                <>
+                    <a 
+                        href={`https://safebooru.donmai.us/posts?tags=${item}`}
+                        target='_new'
+                    >{item}</a>&nbsp;
+                </>
+            ));
+
+        status = <>images with <b>{tagsWithLinks}</b> tags</>
+        }
+
         return (
             <div className="imageGallery" >
-                Loaded {data.length} images<br />
+                Loaded {data.length} {status} (page: {this.state.page})<br />
                 <div>
                     {data.map(item => (
-                        <img 
+                        <a
+                            href={"https://safebooru.donmai.us/posts/" + item.id} 
+                            target="_new" 
+                            key={'link'+item.id}
+                        ><img 
                             src={item.preview_file_url} 
-                            alt={item.id} key={item.id}
+                            alt={item.id}
                             className="thumbnails"
-                        />
+                        /></a>
+                        
                     ))}
                 </div>
             </div>

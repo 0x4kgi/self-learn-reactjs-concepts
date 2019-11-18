a436384b2194db02dae5289543419dfb22c9b083
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2019-11-18 16:56:16 +0800

    updated safebooru demo

--- src/apps/ajax/ajaxTest.js
+++ src/apps/ajax/ajaxTest.js
@@ -6,35 +6,91 @@ class SafebooruAjax extends React.Component {
         super(props);
 
         this.state = {
-            defaultLimit: 10,
-            limit: 10,
+            limit: 20,
+            tags: "scenery no_human",
+            pageNumber: 1,
+
+            key: 0,
+
+            showNext: true,
+            loaded: false,
         };
     }
 
     buttonClick() {
+        this.setState((prevState) => {
+            return { 
+                key: prevState.key + 1,
+                pageNumber: 1,
+                showNext: true,
+                loaded: false,
+            };
+        });
+    }
+
+    handleInputChange(event) {
+        let name = event.target.name;
+        let value = event.target.value;
+
         this.setState({
-            limit: document.getElementById('limitInput').value,
+            [name]: value,
+        });
+    }
+
+    turnPage(value) {
+        this.setState((prevState) => {
+            return {
+                pageNumber: prevState.pageNumber + value,
+                key: prevState.key + 1,
+            };
         });
     }
 
     render() {
         var toggles = (
             <div className="_inputHolder">
-                Limit: (max 200)
+                Tags:
                 <input 
-                    type="text"  
-                    placeholder="enter how many images to load"
-                    defaultValue="10"
-                    id="limitInput"
+                    name="tags"
+                    type="text"
+                    value={this.state.tags}
+                    onChange={(e) => this.handleInputChange(e)}
                 />
-                <button onClick={(e) => this.buttonClick(e)}>Change limit</button>
+                Limit:
+                <input 
+                    name="limit"
+                    type="text"
+                    value={this.state.limit}
+                    onChange={(e) => this.handleInputChange(e)}
+                />
+                <button onClick={(e) => this.buttonClick(e)}>:magnifying glass emoji:</button>
             </div>
         )
+
+        let prevButton = (this.state.pageNumber > 1)
+            && <button onClick={(e) => this.turnPage(-1)}>&lt;</button>;
+        let nextButton = ((this.state.pageNumber) && this.state.showNext)
+            && <button onClick={(e) => this.turnPage(1)}>&gt;</button>;
+
+        let pages =  (
+            <div className="_pageIndicator">
+                page: {prevButton}
+                {this.state.pageNumber}
+                {nextButton}
+            </div>
+        );    
 
         return (
             <div className="text-center">
                 {toggles}
-                <RenderThumbs limit={this.state.limit} key={this.state.limit}/>
+                <RenderThumbs 
+                    tags={this.state.tags}
+                    page={this.state.pageNumber}
+                    limit={this.state.limit} 
+                    key={this.state.key}
+                />
+                <hr />
+                {pages}
             </div>            
         );
     }
